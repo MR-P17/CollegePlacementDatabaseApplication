cf6afaf8fdc85a7498ffa77df976b1107d2f806f
SUMIT KUMAR <sk69@iitbbs.ac.in>
2020-04-27 23:44:06 +0530

    added email-verification feature

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const port = 9999;
+var bcrypt = require("bcrypt");
+nev = require('email-verification')(mongoose);
+const port = process.env.PORT;
+const saltround = 8;
 
 
 mongoose.connect("mongodb://localhost:27017/unipl", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true)
+
 // CONNECTION EVENTS
 // When successfully connected
 mongoose.connection.on('connected', function () {  
@@ -53,42 +59,111 @@ const studentSchema = {
     sdegree:{
         type:String,
         required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
     }
-}
-const Student = mongoose.model("Student", studentSchema);
+};
 
+const Student = mongoose.model("Student", studentSchema), tempStudent = mongoose.model("tempStudent", studentSchema);
+const User = require('./user/userModel');
 
-// main logic starts here
-
-app.get("/", function(req, res){
-    res.render('student');
-})
-app.post("/",function(req, res){
-    // res.send("<p>Succesful submission</p>");
-        let s1 = new Student({
-            sroll: req.body.sroll,
-            sname: req.body.sname,
-            sbranch: req.body.sbranch,
-            sdegree: req.body.sdegree,
+/*
+// sync version of hashing function
+var myHasher = function(password, tempUserData, insertTempUser, callback) {
+    var hash = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    return insertTempUser(hash, tempUserData, callback);
+};
+*/
+// async version of hashing function
+myHasher = function(password, tempUserData, insertTempUser, callback) {
+    bcrypt.genSalt(saltround, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            return insertTempUser(hash, tempUserData, callback);
         });
+    });
+};
 
-        var flg = 0;
-        Student.find({sroll:req.body.sroll}, function(err, studentRecords){
-            if(!err){
-                if(studentRecords.length === 0){
-                    s1.save();
-                }
-            }else{
-                console.log("No data present!");
-            }
-        });
-        
-        setTimeout(function(){
-            res.redirect("/home");
-        }, 2000);
+
+
+// NEV configuration =====================
+nev.configure({
+    // persistentUserModel: User,
+    // expirationTime: 20*60*60, // 10 minutes
+
+    verificationURL: 'http://localhost:' + port + '/verify-email/${URL}',
+    transportOptions: {
+        service: 'Gmail',
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PSWD
+        }
+    },
+
+    verifyMailOptions: {
+        from: 'Do Not Reply <' + process.env.USER + '@gmail.com>',
+        subject: 'Confirm your account',
+        html: '<p>Please verify your account by clicking <a href="${URL}">this link</a>. If you are unable to do so, copy and ' +
+          'paste the following link into your browser:</p><p>${URL}</p>',
+        text: 'Please verify your account by clicking the following link, or by copying and pasting it into your browser: ${URL}'
+      },
+      verifySendMailCallback: function(err, info) {
+        if (err) {
+          throw err;
+        } else {
+          console.log(info.response);
+        }
+      },
+      
+      shouldSendConfirmation: true,
+      confirmMailOptions: {
+        from: 'Do Not Reply <' + process.env.USER + '@gmail.com>',
+        subject: 'Successfully verified!',
+        html: '<p>Your account has been successfully verified.</p>',
+        text: 'Your account has been successfully verified.'
+      },
+      confirmSendMailCallback: function(err, info) {
+        if (err) {
+          throw err;
+        } else {
+          console.log(info.response);
+        }
+      },
+    hashingFunction: myHasher,
+
+    /* mongo-stuff */
+    persistentUserModel: User,
+    // tempUserModel: null,
+    // tempUserCollection: 'temporary_users',
+    emailFieldName: 'email',
+    passwordFieldName: 'password',
+    URLFieldName: 'GENERATED_VERIFYING_URL',
+    expirationTime: 20*60*60,
+    passwordFieldName: 'pw',
+
+}, function(err, options) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('configured: ' + (typeof options === 'object'));
 });
 
 
+nev.generateTempUserModel(User, function(err, tempUserModel) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('generated temp user model: ' + (typeof tempUserModel === 'function'));
+});
+
+
+// main logic starts here
 app.get("/home", function(res,res){
 
     Student.find({}, function(err, studentRecords){
@@ -101,9 +176,52 @@ app.get("/home", function(res,res){
     });
 });
 
-app.post("/AlreayExist", function(req, res){
-    res.redirect("/home");
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + '/public/student_signup.html');
 });
+
+app.get("/login", function(req, res){
+    res.sendFile(__dirname + '/public/student_signin.html');
+});
+
+app.post("/",function(req, res){
+    let email = req.body.email, pswd = req.body.pswd;
+    let temps = new tempStudent({
+        sroll: req.body.sroll,
+        sname: req.body.sname,
+        sbranch: req.body.sbranch,
+        sdegree: req.body.sdegree,
+        email: email
+    });
+    let user = new User({
+        email: email,
+        pw: pswd
+    });
+
+    // email verification function 
+    evf(user, email, res, temps);
+ 
+});
+
+app.post("/login", function(req, res){
+    let email = req.body.email, pswd = req.body.pswd;
+    User.find({email: email}, function(err, record){
+        if(!err){
+            bcrypt.compare(pswd, record[0].pw, function(err, result) {
+                if(result){
+                    res.redirect("/home");
+                }else{
+                    res.redirect("/login");
+                }
+            });
+        
+        }else{
+        }
+    })
+});
+
+
 
 app.get("/company", function(req, res){
     res.render('company');
@@ -118,6 +236,103 @@ app.get("/offer", function(req, res){
 });
 
 
+// user accesses the link that is sent to user
+app.get("/verify-email/:URL", function(req, res) {
+    var url = req.params.URL;
+
+    nev.confirmTempUser(url, function(err, user) {
+        if (user) {
+            let email = user.email;
+            tempStudent.find({email:email}, function(err, record){
+                if(!err){
+                    let s = new Student({
+                        sroll: record[0].sroll,
+                        sname: record[0].sname,
+                        sbranch: record[0].sbranch,
+                        sdegree: record[0].sdegree,
+                        email: record[0].email
+                    });
+                    s.save();
+                }
+            });
+            tempStudent.deleteOne({email:email}, function(err){
+                if(!err){
+                    console.log("Suceessfully deleted");
+                }
+            });
+
+            res.sendFile(__dirname + '/public/signin_response.html');
+
+            // not neccessary for user
+            nev.sendConfirmationEmail(user.email, function(err, info) {
+                if (err) {
+                    // return res.status(404).send('ERROR: sending confirmation email FAILED');
+                    console.log('ERROR: sending confirmation email FAILED');
+                }
+                
+            });
+        } else {
+            res.status(404).send('INTERNAL ERROR: confirmation failed, either signUp again or signIn');
+            console.log('ERROR: confirming temp user FAILED');
+        }
+    });
+});
+
+
+// email-verification function
+const evf = function(newUser, email, res, temps){
+
+    nev.createTempUser(newUser, function(err, existingPersistentUser, newTempUser) {
+        if (err) {
+            console.log("ERROR: creating temp user FAILED");
+            return res.status(404).send('Internal Error!');
+        }
+
+        // user already exists in persistent collection
+        if (existingPersistentUser) {
+            res.send("<p>You have already signed up and confirmed your account. Did you forget your password?</p>");
+        }
+
+        
+        // new user created
+        if (newTempUser) {
+            var URL = newTempUser[nev.options.URLFieldName];
+            temps.save();
+            nev.sendVerificationEmail(email, URL, function(err, info) {
+                if (err) {
+                    console.log("ERROR: sending verification email FAILED");
+                    return res.status(404).send('Email can not sent, Internal Error!');
+                }
+                // save the user details in temp location
+                res.send("<p>An email has been sent to you. Please check it to verify your account.</p>");
+            });
+
+            // user already exists in temporary collection!
+        } else {
+
+            res.send("<p>You have already signed up. Please check your email to verify your account.</p>")
+        }
+    });
+
+}
+
+const rve = function(email){
+    nev.resendVerificationEmail(email, function(err, userFound) {
+        if (err) {
+            return res.status(404).send('ERROR: resending verification email FAILED');
+        }
+        if (userFound) {
+            res.json({
+                msg: 'An email has been sent to you, yet again. Please check it to verify your account.'
+            });
+        } else {
+            res.json({
+                msg: 'Your verification code has expired. Please sign up again.'
+            });
+        }
+    });
+
+}
 
 
 
--- public/index.js
+++ public/index.js
@@ -1,20 +1,52 @@
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        var form1 = document.getElementById("Ae");
-        // forms.pop();
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-                    form.addEventListener('submit', function(event) {
-                        if (form.checkValidity() === false) {
-                            event.preventDefault();
-                            event.stopPropagation();
-                        }
-                        form.classList.add('was-validated');
-                    }, false);
-        });
-    }, false);
 
-})();
+'use strict';
+
+const blockfun = function(event){
+    event.preventDefault();
+    event.stopPropagation();
+}
+window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        blockfun(event);
+                    }
+                    
+                    form.classList.add('was-validated');
+                }, false /*optional bubbling/caputring */);
+    });
+
+}, false/*optional bubbling/caputring */);
+
+
+
+
+
+
+// const check_form = function(event){
+    
+    // var name, roll;
+    // var regE = /^[0-9A-Za-z]+$/i, regE1 = /^[A-Za-z]+$/i;
+    // name = form.sname.value;
+    // roll = form.sroll.value;
+    // email = form.email.value;
+
+    // if(!regE1.test(name)){
+        /* apply custome css */
+    //     blockfun(event);
+    // }
+
+    // else if(!regE.test(roll)){
+        /* apply custome css */
+    //     blockfun(event);
+    // }else{
+    // }
+
+// }
+
+// var forms = document.getElementsByClassName("needs-validation");
+// forms[0].addEventListener("submit", check_form);
